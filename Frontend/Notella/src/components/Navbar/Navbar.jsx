1809e4d2d4a26190136e5d6e1f387fdac57277f3
Kaumi Nethma <kauminethma@gmail.com>
2025-03-24 01:57:09 +0530

    Add ProfileInfo and SearchBar components, implement email validation, and update styles

--- Frontend/Notella/src/components/Navbar/Navbar.jsx
+++ Frontend/Notella/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ProfileInfo from '../Cards/profileInfo';
+import { useNavigate } from 'react-router-dom';
+import SearchBar from '../SearchBar/SearchBar';
 
 const Navbar = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const Navigate = useNavigate();
+  const handleSearch = () => { };
+
+  const onClearSearch = () => {
+    setSearchQuery("");
+  };
+  const onLogout = () => {
+    Navigate("/login");
+  }
   return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
       <h2 className="text-xl font-medium text-balck py-2">Notella</h2>
+      <SearchBar
+        value={searchQuery}
+        onChange={({ target }) => {
+          setSearchQuery(target.value);
+        }}
+        handleSearch={handleSearch}
+        onClearSearch={onClearSearch} />
+      <ProfileInfo onLogout={onLogout} />
     </div>
   );
 };
