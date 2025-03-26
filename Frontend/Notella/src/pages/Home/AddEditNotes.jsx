38d8f3b5f1d34cb94e5d75b2e05ff1d80eee8b96
Kaumi Nethma <kauminethma@gmail.com>
2025-03-26 23:38:19 +0530

    Implement AddEditNotes component with title, content, and tag input functionality

--- Frontend/Notella/src/pages/Home/AddEditNotes.jsx
+++ Frontend/Notella/src/pages/Home/AddEditNotes.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import TagInput from '../../components/Input/TagInput'
+import { MdClose } from 'react-icons/md';
+const AddEditNotes = ({onClose}) => {
 
-const AddEditNotes = () => {
+    const [title,setTitle] = useState("");
+    const [content,setContent] = useState("");
+    const [tags,setTags] = useState([]);
     return (
-        <div>
+
+        
+        <div className='relative'>
+            <button className='w-10 h-10 rounded-full flex items-center justify-center absolute -top-3 -right-3 hover:bf-slate-50 ' onClick={onClose}>
+                <MdClose className="text-xl text-slate-400"></MdClose>
+            </button>
             <div className="flex flex-col gap-2">
                 <label className="input-label">TITLE</label>
                 <input
                     type="text"
                     className="text-2xl text-slate-950 outline-none"
                     placeholder='Go to gym at 5pm'
+                    value={title}
+                    onChange={({target}) => setTitle(target.value)} 
                 />
             </div>
 
@@ -19,10 +31,13 @@ const AddEditNotes = () => {
                     className="text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded"
                     placeholder='Content'
                     rows={10}
+                    value={content}
+                    onChange={({target}) => setContent(target.value)}
                 />
             </div>
             <div className='mt-3'>
                 <label className='input-label'>TAGS</label>
+                <TagInput tags={tags} setTags={setTags}/>
             </div>
 
             <button className='btn-primary font-medium mt-5 p-3' onClick={() =>{}}>
--- Frontend/Notella/src/pages/Home/Home.jsx
+++ Frontend/Notella/src/pages/Home/Home.jsx
@@ -53,7 +53,14 @@ onClick={() => {
     }}
     contentLabel=""
     className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5  ">
-       <AddEditNotes />
+       <AddEditNotes 
+       onClose={() => {
+        setOpenAddEditModal({
+          isShown:false,
+          type:"add",
+          data:null
+        });
+       }}/>
   </Modal>
     </> 
   );
